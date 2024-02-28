6b7d38b81e53e8a9346e1a951adf375de47a6ad2
Taner <taner@taner-k.nl>
2024-02-28 14:12:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Baby weather app
+//If rain 'Grab your umbrella :umbrella'
+//else 'Wear your sunglasses :sunglasses'
+
+let weather = prompt('How is the weather?')
+
+if (weather == 'rain')
+  console.log('Grab your umbrella :umbrella')
+else {
+  console.log('Wear your sunglasses :sunglasses')
+}
